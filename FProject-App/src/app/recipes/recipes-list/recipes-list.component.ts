be22a45e9aa090d7c67a9a321a99bd67b7cbabe5
Naeem Hisham <naiemhisham@gmail.com>
2022-10-28 19:59:26 +0200

    component & data binding

--- FProject-App/src/app/recipes/recipes-list/recipes-list.component.ts
+++ FProject-App/src/app/recipes/recipes-list/recipes-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../recipe.model';
 
 @Component({
   selector: 'app-recipes-list',
@@ -6,6 +7,9 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recipes-list.component.css']
 })
 export class RecipesListComponent implements OnInit {
+  recipes: Recipe[] = [
+    new Recipe('Recipe 1', 'Test Desc','https://assets.bonappetit.com/photos/61b775620fb3fcc4cbf036c1/master/pass/20211208%20Spaghetti%20Squash%20with%20Tomato%20Sauce%20and%20Mozarella%20LEDE.jpg')
+  ];
 
   constructor() { }
 
--- first-projrct/src/app/app.component.ts
+++ first-projrct/src/app/app.component.ts
@@ -13,5 +13,23 @@ import { Component } from '@angular/core';
 //`]
 })
 export class AppComponent {
+  serverElements = [{type:'server',name:'Testserver',content:'Just a test!'}];
+
   title = 'first-project';
+
+  onServerAdded(serverData: { serverName: string, serverContent: string }) {
+    this.serverElements.push({
+      type: 'server',
+      name: serverData.serverName,
+      content: serverData.serverContent
+    });
+  }
+
+  onBlueprintAdded(blueData: { serverName: string, serverContent: string }) {
+    this.serverElements.push({
+      type: 'blueprint',
+      name: blueData.serverName,
+      content: blueData.serverContent
+    });
+  }
 }
